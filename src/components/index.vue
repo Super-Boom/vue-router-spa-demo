7a72691136ef5ae22bf4952a7b5d05fddc2dee12
Super-Boom <794489107@qq.com>
2019-04-10 00:08:48 +0800

    add func

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,10 +1,14 @@
 <template>
-    <div>1</div>
+    <div @click="func">1</div>
 </template>
 
 <script>
 export default {
-
+    methods:{
+        func(){
+            this.$emit('func');
+        }
+    }
 }
 </script>
 
